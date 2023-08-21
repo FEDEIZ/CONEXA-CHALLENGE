396e7359c57a23817fa7f8d5f117711b1a8d3128
Federico Interlandi Zoireff <70079186+FEDEIZ@users.noreply.github.com>
2023-08-21 09:34:37 -0300

    simple app

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,29 @@ import { ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan'
 
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['debug', 'log'],
-  });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
-  app.use(morgan('combined'));
-  await app.listen(3000);
-  console.log("Nest app listening on port 3000")
-}
-bootstrap();
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, {
+//     logger: ['debug', 'log'],
+//   });
+//   app.useGlobalPipes(new ValidationPipe({
+//     whitelist: true,
+//     forbidNonWhitelisted: true,
+//   }));
+//   app.use(morgan('combined'));
+//   await app.listen(3000);
+//   console.log("Nest app listening on port 3000")
+// }
+// bootstrap();
+
+import * as http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
