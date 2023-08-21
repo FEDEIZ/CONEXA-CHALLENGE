58b1923bec44cbc7e8cfa2814406d2189efc76e6
Federico Interlandi Zoireff <70079186+FEDEIZ@users.noreply.github.com>
2023-08-21 10:06:36 -0300

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'log'],
   });
-  app.use(cors());
+  // app.use(cors());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, HEAD, PUT, POST, DELETE, PATCH',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  });
+  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
-  app.use(morgan('combined'));
+  app.use(morgan('common'));
   
   await app.listen(process.env.PORT);
   console.log("Nest app listening on port " + process.env.PORT || 8000)
